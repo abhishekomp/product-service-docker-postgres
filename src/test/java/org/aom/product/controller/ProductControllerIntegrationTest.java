68ba376439bfcc47baf6cb142305d16938f5d755
abhishekomp <abhishek2283@hotmail.com>
2024-09-01 15:16:34 +0200

    added testcontainer for postgreSQL db

---
+++ src/test/java/org/aom/product/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package org.aom.product.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+class ProductControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Container
+    static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
+    @Test
+    void getAllProducts() throws Exception {
+        mockMvc.perform(get("/product-service/getAllProducts"))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/org/aom/product/repository/ProductRepositoryTestContainerManualStartTest.java
@@ -0,0 +1,60 @@
+package org.aom.product.repository;
+
+import jakarta.persistence.EntityManager;
+import org.aom.product.model.Product;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductRepositoryTestContainerManualStartTest {
+
+    //static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14-alpine"));
+    static PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgresqlContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgresqlContainer.stop();
+    }
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
+    @Test
+    void findAllSamsungProducts() {
+
+        entityManager.persist(new Product("HUW2024", "HUW2024"));
+        entityManager.persist(new Product("Samsung Majestic", "SAM2024"));
+        entityManager.persist(new Product("Samsung Fold", "SAM2024"));
+
+        List<Product> allSamsungProducts = productRepository.findAllSamsungProducts();
+        assertThat(allSamsungProducts).hasSize(3);
+    }
+}
